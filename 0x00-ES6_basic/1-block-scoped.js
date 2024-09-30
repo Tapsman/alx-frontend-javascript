ab5ebe0203b25133ce1fbf186c91f8fcb6a90829
Tapsman <tapiwanyoni140@gmail.com>
2024-09-30 09:56:12 -0700

    1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,6 +3,7 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
+    /*And now to disable unused vars*/
     const task = true;
     const task2 = false;
   }
