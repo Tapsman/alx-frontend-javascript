6f929f2bfbaae89c8df66275573f2a49a2ed5b31
Tapsman <tapiwanyoni140@gmail.com>
2024-09-30 14:32:18 -0700

    101-iterateThroughObject.js

---
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,7 @@
+export default function iterateThroughObject(reportWithIterator) {
+  const retString = [];
+  for(const report of reportWithIterator)
+    retString.push(report);
+  }
+  return retString.join(' | ');
+}
