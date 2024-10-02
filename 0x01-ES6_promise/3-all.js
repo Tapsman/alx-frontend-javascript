6afe71b1057678c5f5676b8dbac985075f5d8b4b
Tapsman <tapiwanyoni140@gmail.com>
2024-10-02 02:58:17 -0700

    3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,6 +4,6 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignUp() {
   return Promise
     .all([uploadPhoto(), createUser()])
-    .then((values) => console.log('${values[0].body ${values[1].firstName} ${values[1].lastName}'))
+    .then((values) => console.log('${values[0].body} ${values[1].firstName} ${values[1].lastName}'))
     .catch(() => console.log('Signup system offline'));
 }
