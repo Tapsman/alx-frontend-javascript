89bf8dd1c1ae7d28e8429190151d6f2b85022d86
Tapsman <tapiwanyoni140@gmail.com>
2024-10-02 04:09:55 -0700

    5-photo-reject.js

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,4 +1,3 @@
 export default function uploadPhoto(filename) {
-  /* eslint-disable no-unused-vars */
-  return Promise.reject(Error('${filename} cannot be processed'));
+  return Promise.reject(Error(`${filename} cannot be processed`));
 }
