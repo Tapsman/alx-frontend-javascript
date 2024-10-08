659fa9d7c7438da9f306a86d9e118c79daada306
Tapsman <tapiwanyoni140@gmail.com>
2024-10-08 11:05:12 -0700

    100-weak.js

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -3,6 +3,6 @@ const weakMap = new WeakMap();
 const queryAPI = (endpoint) => {
   const count = (weakMap.get(endpoint) || 0) + 1;
   if (count >= 5) throw new Error('Endpoint load is high');
-  weakmap.set(endpoint, count);
+  weakMap.set(endpoint, count);
 };
 export { weakMap, queryAPI };
